a9d55b7fe1916a0154114d25eab1fb4089666495
jbarcia <>
2023-05-16 19:08:55 -0400

    finished challenged, verified working

--- aws/challenges/double_tap/double_tap.tf
+++ aws/challenges/double_tap/double_tap.tf
@@ -2,92 +2,153 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create Starting Role and Permissions
-resource "aws_iam_role" "double_tap" {
-  name = "double_tap"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          AWS = var.ctf_starting_user_arn
-        }
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "double_tap_iam_policy" {
-  name        = "double_tap_iam_policy"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect   = "Allow"
-      Action   = ["lambda:CreateFunction", "iam:PassRole"]
-      Resource = "*"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "double_tap_iam_policy_attachment" {
-  policy_arn = aws_iam_policy.double_tap_iam_policy.arn
-  role       = aws_iam_role.double_tap.name
-}
-
-resource "aws_iam_policy" "lambda_ec2_policy" {
-  name        = "lambda_ec2_policy"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "ec2:*"
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "aws:ResourceTag/double_tap" = "true"
-          }
-        }
-      }
-    ]
-  })
-}
-
-
-resource "aws_iam_role" "lambda_ec2" {
-  name = "lambda_ec2"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Service = "lambda.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_ec2_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  role       = aws_iam_role.lambda_ec2.name
-}
-
 # Configure EC2
+resource "aws_security_group" "instance_sg" {
+  name        = "instance_sg"
+  description = "Security group for EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+  owners = ["amazon"] 
+}
+
+# Create flag
+resource "aws_secretsmanager_secret" "double_tap_flag_secret" {
+  name = "DT_flag"
+  recovery_window_in_days = 0
+}
+
+# Configure flag secret
+resource "aws_secretsmanager_secret_version" "double_tap_flag_secret_version" {
+  secret_id     = aws_secretsmanager_secret.double_tap_flag_secret.id
+  secret_string = "12345"
+}
+
+resource "aws_iam_policy" "double_tap_secret_policy" {
+  name        = "double_tap_secret_policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecretVersionIds",
+          "secretsmanager:ListSecrets"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          aws_secretsmanager_secret.double_tap_flag_secret.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "double_tap_secret" {
+  name = "double_tap_secret"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "double_tap_secret_policy" {
+  policy_arn = aws_iam_policy.double_tap_secret_policy.arn
+  role       = aws_iam_role.double_tap_secret.name
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_ec2_secret_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.double_tap_secret.name
+}
+
+resource "aws_instance" "flag" {
+  timeouts {
+    create = "1h"
+    update = "30m"
+    delete = "30m"
+  }
+
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.double_tap_secret_profile.name}"  
+  security_groups = ["instance_sg"]
+  tags = {
+    double_tap2  = "true",
+    Name = "double_tap"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'Starting SSM agent installation...'
+              sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+              echo 'SSM agent installation complete.'
+              echo 'Starting SSM agent...'
+              sudo systemctl start amazon-ssm-agent
+              echo 'SSM agent started.'
+              EOF
+}
+
+resource "aws_iam_instance_profile" "double_tap_secret_profile" {
+  name = "double_tap_secret_profile"
+  role = aws_iam_role.double_tap_secret.name
+}
+
+# Create first hop
 resource "aws_iam_policy" "ec2_privileged_policy" {
   name        = "ec2_privileged_policy"
   policy      = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "ssm:*"
-        Resource = "*"
-      }
+        {
+            "Effect": "Allow",
+            "Action": ["ssm:StartSession"],
+            "Resource": "*",
+            "Condition": {"StringEquals": {"aws:ResourceTag/double_tap2" = "true"}}
+        },
+        {
+            "Effect": "Deny",
+            "Action": ["ec2:DeleteTags", "ec2:CreateTags"],
+            "Resource": "*"
+        },
+        {
+         "Effect": "Allow",
+         "Action": "ssm:*",
+         "Resource": "arn:aws:ssm:*:*:*"
+        },
+        {
+         "Effect":"Allow",
+         "Action":["ssm:SendCommand"],
+         "Resource":["arn:aws:ec2:*:*:instance/*"],
+         "Condition": {"StringEquals": {"aws:ResourceTag/double_tap2" = "true"}}
+        }
     ]
   })
 }
@@ -112,12 +173,26 @@ resource "aws_iam_role_policy_attachment" "ec2_privileged_policy" {
   role       = aws_iam_role.ec2_privileged.name
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_ec2_priv_policy_attachment" {
+  role       = aws_iam_role.ec2_privileged.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_instance" "ec2" {
-  ami           = "ami-0ff8a91507f77f867"
+  timeouts {
+    create = "1h"
+    update = "30m"
+    delete = "30m"
+  }
+
+  ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
   iam_instance_profile = "${aws_iam_instance_profile.ec2_privileged_profile.name}"
+  security_groups = ["instance_sg"]
+
   tags = {
-    double_tap  = "true"
+    double_tap1  = "true",
+    Name = "double_tap"
   }
 }
 
@@ -126,19 +201,87 @@ resource "aws_iam_instance_profile" "ec2_privileged_profile" {
   role = aws_iam_role.ec2_privileged.name
 }
 
-# Create flag
-resource "aws_instance" "flag" {
-  ami           = "ami-0ff8a91507f77f867"
-  instance_type = "t2.micro"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo 'Starting SSM agent installation...'
-              sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-              echo 'SSM agent installation complete.'
-              echo 'Starting SSM agent...'
-              sudo systemctl start amazon-ssm-agent
-              echo 'SSM agent started.'
-              echo "test" > /root/flag.txt
-              EOF
+# Create Starting Role and Permissions
+
+########################################## 
+######          CHANGE THIS         ######
+##########################################
+resource "aws_iam_role" "double_tap" {
+  name = "double_tap"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          AWS = "*"
+        }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "double_tap_iam_policy" {
+  name        = "double_tap_iam_policy"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["lambda:CreateFunction", "lambda:InvokeFunction", "iam:PassRole"]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "double_tap_iam_policy_attachment" {
+  policy_arn = aws_iam_policy.double_tap_iam_policy.arn
+  role       = aws_iam_role.double_tap.name
+}
+
+resource "aws_iam_policy" "lambda_ec2_policy" {
+  name        = "lambda_ec2_policy"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "ec2:*"
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:ResourceTag/double_tap1" = "true"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role" "lambda_ec2" {
+  name = "lambda_ec2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ec2_policy" {
+  policy_arn = aws_iam_policy.lambda_ec2_policy.arn
+  role       = aws_iam_role.lambda_ec2.name
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ec2_read_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+  role       = aws_iam_role.lambda_ec2.name
 }
