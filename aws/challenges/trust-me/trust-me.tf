7a073fceababbb275138ba814df28105c10285b6
sethsec-bf <46326948+sethsec-bf@users.noreply.github.com>
2023-10-05 09:11:27 -0400

    remove debug os.system call as it is not the intended solution to uncomment it and use it

--- aws/challenges/trust-me/trust-me.tf
+++ aws/challenges/trust-me/trust-me.tf
@@ -28,7 +28,7 @@ EOF
 // create an identity provider for the github OIDC flow
 resource "aws_iam_openid_connect_provider" "github" {
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
   url = "https://token.actions.githubusercontent.com"
 }
 
