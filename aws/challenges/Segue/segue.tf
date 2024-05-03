e16a770b2bd233e82c4db936a809daf3aea79250
bf-singularity <gkleijn@bishopfox.com>
2024-05-03 13:35:25 -0700

    changes based on playtesting feedback

--- aws/challenges/Segue/segue.tf
+++ aws/challenges/Segue/segue.tf
@@ -1,4 +1,4 @@
-//create a policy that has security audit permissions as well as iam:assumeRole:* and read/get permissions to deployment_automation S3 bucket
+//create a policy that has security audit permissions as well as iam:assumeRole for deployment_automation and read/get permissions to deployment_automation S3 bucket
 resource "aws_iam_policy" "authorized_deployers" {
   name        = "authorized_deployers"
   path        = "/"
@@ -12,7 +12,10 @@ resource "aws_iam_policy" "authorized_deployers" {
         "Action" : [
           "sts:AssumeRole"
         ],
-        "Resource" : "*"
+        "Resource" : [
+          "arn:aws:iam::${var.account_id}:role/deployment_automation",
+          "arn:aws:iam::992382614597:role/*"
+        ]
       },
       {
         "Effect" : "Allow",
@@ -60,20 +63,20 @@ resource "aws_iam_role_policy_attachment" "security_audit-attachment" {
 }  
 
 //create segue-flag in secretsmanager
-resource "aws_secretsmanager_secret" "segue-flag" {
-  name                    = "segue-flag"
+resource "aws_secretsmanager_secret" "SegueFlag" {
+  name                    = "SegueFlag"
   recovery_window_in_days = 0
   tags = {
     Name = "Segue flag"
   }
 }
 
-resource "aws_secretsmanager_secret_version" "segue-flag" {
-  secret_id     = aws_secretsmanager_secret.segue-flag.id
+resource "aws_secretsmanager_secret_version" "SegueFlag" {
+  secret_id     = aws_secretsmanager_secret.SegueFlag.id
   secret_string = "Thank you, but your flag is in another castle"
 }
 
-//create a policy that has permissions to read flag from secretsmanager
+//create a policy for deployment_automation that has permissions to assume the secret_reader role
 resource "aws_iam_policy" "deployment_automation" {
   name        = "deployment_automation"
   path        = "/"
@@ -85,16 +88,9 @@ resource "aws_iam_policy" "deployment_automation" {
       {
         "Effect" : "Allow",
         "Action" : [
-          "secretsmanager:ListSecrets"
+          "sts:AssumeRole"
         ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "secretsmanager:GetSecretValue"
-        ],
-        "Resource" : aws_secretsmanager_secret.segue-flag.arn
+        "Resource" : "arn:aws:iam::${var.account_id}:role/secret_reader"
       }
     ]
   })
@@ -118,10 +114,61 @@ resource "aws_iam_role" "deployment_automation" {
   })
 }
 
-//attach the policy to the role
+//attach the deployment_automation policy to the deployment_automation role
 resource "aws_iam_role_policy_attachment" "deployment_automation-attachment" {
   role       = aws_iam_role.deployment_automation.name
   policy_arn = aws_iam_policy.deployment_automation.arn
+}
+
+//create a policy that has permissions to read flag from secretsmanager
+resource "aws_iam_policy" "secret_reader" {
+  name        = "secret_reader"
+  path        = "/"
+  description = "policy for reading secrets"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "secretsmanager:ListSecrets"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "secretsmanager:GetSecretValue"
+        ],
+        "Resource" : aws_secretsmanager_secret.SegueFlag.arn
+      }
+    ]
+  })
+}
+
+// create role to read secrets
+resource "aws_iam_role" "secret_reader" {
+  name                = "secret_reader"
+  assume_role_policy  = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          "AWS": ["arn:aws:iam::${var.account_id}:role/deployment_automation"]
+        }
+      },
+    ]
+  })
+}
+
+//attach the secret_reader policy to the secret_reader role
+resource "aws_iam_role_policy_attachment" "secret_reader-attachment" {
+  role       = aws_iam_role.secret_reader.name
+  policy_arn = aws_iam_policy.secret_reader.arn
 }
 
 //create S3 bucket
@@ -150,6 +197,13 @@ resource "aws_s3_bucket_public_access_block" "deployment-automation-public-acces
 // upload a file to the bucket.
 resource "aws_s3_object" "deployment-automation-object" {
   bucket = aws_s3_bucket.deployment-automation.id
-  key    = "Organizational Structure.pdf"
-  source = "challenges/Segue/data/s3/Organizational Structure.pdf"
+  key    = "Security Review Process.pdf"
+  source = "challenges/Segue/data/s3/Security Review Process.pdf"
+}
+
+// upload another file to the bucket.
+resource "aws_s3_object" "deployment-automation-object-2" {
+  bucket = aws_s3_bucket.deployment-automation.id
+  key    = "Deployment Process Description.pdf"
+  source = "challenges/Segue/data/s3/Deployment Process Description.pdf"
 }
